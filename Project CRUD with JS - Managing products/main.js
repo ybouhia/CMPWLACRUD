5ddea8155aa8f264b07337d5a27e62f669c56f69
ybouhia <126120506+ybouhia@users.noreply.github.com>
2023-02-22 23:42:32 +0100

    Add files via upload

---
+++ Project CRUD with JS - Managing products/main.js
@@ -0,0 +1,57 @@
+
+let title = document.getElementById('title');
+let price = document.getElementById('price');
+let taxes = document.getElementById('taxes');
+let ads = document.getElementById('ads');
+let discount = document.getElementById('discount');
+let total = document.getElementById('total');
+let count = document.getElementById('count');
+let category = document.getElementById('category');
+let submit = document.getElementById('submit');
+
+// get total 
+function gettotal() 
+{
+    if(price.value !=" ")
+    {
+        let result = (+price.value + +taxes.value + 
+            +ads.value - +discount.value);
+        total.innerHTML = result;
+        total.style.background ="#040";
+        }else { total.innerHTML = ' ';
+        total.style.background = '#a00d02';
+    }
+}
+// create product
+let dataPro = [];
+
+if (localStorage.product !=null){
+    dataPro = JSON.parse(localStorage.product)
+}else{dataPro = [];}
+
+
+submit.onclick = function(){
+    let newPro = {
+        title:title.value,
+        price:price.value,
+        taxes:taxes.value,
+        ads:ads.value,
+        discount:discount.value,
+        total:total.innerHTML,
+        count:count.value,
+        category:category.value,
+    }
+    dataPro.push(newPro)
+    localStorage.setItem('product', JSON.stringify(dataPro))
+    console.log(newPro)
+}
+
+// save local storage
+// clear inputs 
+//////////////////////
+// read 
+// count 
+/////////////////////
+// delete
+// update 
+// search
